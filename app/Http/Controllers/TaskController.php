0f1315c462b56aedbfa33435d962367953cf4073
asamin <applevr58@gmail.com>
2018-05-03 11:06:55 +0000

    tasklist

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -62,7 +62,11 @@ class TaskController extends Controller
      */
     public function show($id)
     {
-        //
+        $task = Task::find($id);
+
+        return view('task.show', [
+            'task' => $task,
+        ]);
     }
 
     /**
