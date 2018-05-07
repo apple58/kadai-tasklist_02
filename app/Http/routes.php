1d58ba7c58304b909991ee8e7faef3338b1ba35b
asamin <applevr58@gmail.com>
2018-05-07 11:45:56 +0000

    L.12 tasklist modify

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,13 +9,23 @@
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
+Route::get('/', 'TaskController@index');
 */
 
-Route::get('/', 'TaskController@index');
+Route::get('/', 'WelcomeController@index');
 
 Route::resource('tasks', 'TaskController');
+
+// ユーザ登録
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
 
 // ログイン認証
 Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
 Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
 Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
+});
